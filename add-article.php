85bdbd5249346e664b07ebd8e6de99cdca822ab5
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-01-08 18:06:40 +0700

    update add new article form

--- add-article.php
+++ add-article.php
@@ -1,7 +1,14 @@
 <?php
 
 require 'includes/database.php';
-require 'includes/header.php';
+
+// Create an array to store error messages
+$errors = [];
+
+// Init variables to avoid warning message
+$post_title = ''; 
+$post_content = ''; 
+$post_publish_date = '';
 
 if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
   $conn = get_db();
@@ -9,28 +16,49 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
   $post_title = $_POST['title'];
   $post_content = $_POST['content'];
   $post_publish_date = $_POST['published_at'];
-  
-  $sql = "INSERT INTO article (title, content, published_at) 
-          VALUES (?, ?, ?)";
 
-  $prepared_statement = mysqli_prepare($conn, $sql);  
+  // Validate if title and content are empty
+  if ( $post_title == '' ) {
+    $errors[] = 'Title is required';
+  }
 
-  if ( $prepared_sql === false ) {
+  if ( $post_content == '' ) {
+    $errors[] = 'Content is required';
+  }
 
-    echo mysqli_error($conn);
+  // For publish date, it is not required, so we need to check that if it has value, we need to validate if the date is correct format
+  if ( $post_publish_date != '' ) {
+    $post_publish_date = str_replace('T', ' ', $post_publish_date);
+    $date_time = date_create_from_format("Y-m-d H:i", $post_publish_date);
 
-  } else {
 
-    mysqli_stmt_bind_param( $prepared_statement, "sss", $post_title, $post_content, $post_publish_date );
-
-    if ( mysqli_stmt_execute( $prepared_statement ) ) {
-      $id = mysqli_insert_id($conn);
-      echo $id;
-    } else {
-      echo mysqli_stmt_error( $prepared_statement );
+    if ( $date_time == false ) {
+      $errors[] = 'Date time is invalid';
+      var_dump($date_time);
+      var_dump($post_publish_date);
     }
 
   }
+
+  if ( empty( $errors ) ) {
+    $sql = "INSERT INTO article (title, content, published_at) VALUES (?, ?, ?)";
+    $prepared_statement = mysqli_prepare($conn, $sql);  
+
+    if ( $prepared_sql === false ) {
+      echo mysqli_error($conn);
+    } else {
+      mysqli_stmt_bind_param( $prepared_statement, "sss", $post_title, $post_content, $post_publish_date );
+  
+      if ( mysqli_stmt_execute( $prepared_statement ) ) {
+        $id = mysqli_insert_id($conn);
+        echo $id;
+      } else {
+        echo mysqli_stmt_error( $prepared_statement );
+      }
+  
+    }
+  }
+
 }
 
 
@@ -38,16 +66,28 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 
 <h1>Add New Article</h1>
 
+<ul>
+  <?php 
+    foreach( $errors as $error_message ) {
+      ?>
+        <li><?php echo $error_message; ?></li>
+      <?php
+    }
+  ?>
+</ul>
+
 <form method="post">
 
   <div class="title">
     <label for="title">Title</label>
-    <input type="text" name="title" id="title">
+    <input type="text" name="title" id="title" value="<?php echo $post_title ?>">
   </div>
 
   <div class="title">
     <label for="content">Content</label>
-    <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    <textarea name="content" id="content" cols="30" rows="10">
+      <?php echo $post_content; ?>
+    </textarea>
   </div>
 
   <div class="datetime">
